5608cb90899b6261bdb3f9d819f129aaf1d8e390
Luqitas <89209298+Luqitas@users.noreply.github.com>
2021-08-19 13:59:41 -0300

    Update patasxd.js

--- fakeapixd/patasxd.js
+++ fakeapixd/patasxd.js
@@ -1,4 +1,4 @@
 [
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZEY4qQmwAJhKVFlypt0nZNgzrkApOax6vew&usqp=CAU"},
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfyqYAyrxqYswPhrZIfJz_wvS_56-zpPoWqg&usqp=CAU"}
+{"result": "https://pm1.narvii.com/7869/c65e89237b7040d465b2190cf2deb4d2370aeee2r1-850-1159v2_hq.jpg"}
 ]
