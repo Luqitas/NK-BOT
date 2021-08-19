914c03134ba52a7ddc5bc70eee0c19f3c7c788ba
Luqitas <89209298+Luqitas@users.noreply.github.com>
2021-08-19 13:57:46 -0300

    Update muslosss.js

--- fakeapixd/muslosss.js
+++ fakeapixd/muslosss.js
@@ -1,4 +1,4 @@
 [
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvMRPeHtlg3wipWxCS055lBMcQpYn2Pogdyw&usqp=CAU"},
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZwjGF4urnf7AI5zVNIaZvbGHtt3u--CzBDw&usqp=CAU"}
+{"result": "https://64.media.tumblr.com/cd3f2bdf7f07e82874ebb289a101c5c5/d695de55c82cf6db-13/s640x960/479b3740d45b25c0fd1fcd5d132e6ff04b12ff3b.jpg"},
+{"result": "https://i.pinimg.com/550x/9a/03/5b/9a035b9b018e27f2510bec6704508392.jpg"}
 ]
